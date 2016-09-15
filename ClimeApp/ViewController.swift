bca64dcd28b42f7ed830a98c4520f44874efccac
Lydia Khashina <adatheleader@gmail.com>
2016-09-15 16:49:20 +0300

    Initial Commit

---
+++ ClimeApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ClimeApp
+//
+//  Created by Лидия Хашина on 15.09.16.
+//  Copyright © 2016 Lydia Khashina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
